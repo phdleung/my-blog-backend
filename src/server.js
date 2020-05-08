728aa3871c8d26747214858653569e6a0b9068f4
Pedro Leung <pedro_leung@hotmail.com>
2020-05-08 23:22:22 +1000

    apis

--- src/server.js
+++ src/server.js
@@ -1,16 +1,37 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+const articlesInfo = {
+  'learn': {
+    upvotes: 0,
+    comments: []
+  },
+  'fast': {
+    upvotes: 0,
+    comments: []
+  },
+  'quick': {
+    upvotes: 0,
+    comments: []
+  }
+};
+
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/hello', (req, res) => {
-  res.send('Hi!');
+app.post('/api/articles/:name/upvote', (req, res) => {
+  const articleName = req.params.name;
+  articlesInfo[articleName].upvotes += 1;
+  res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes!`);
 });
 
-app.post('/hello', (req, res) => {
-  res.send(`Hello ${req.body.name}`);
+app.post('/api/articles/:name/add-comment', (req, res) => {
+  const { username, text } = req.body;
+  const articleName = req.params.name;
+  articlesInfo[articleName].comments.push({ username, text });
+
+  res.status(200).send(articlesInfo[articleName].comments);
 });
 
 app.listen(8000, () => console.log('Listening on port 8000'));
